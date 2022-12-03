ae58beb2a03b95b76767e642f3f9257a590fa155
pulell-af <119008536+pulell-af@users.noreply.github.com>
2022-12-03 18:40:11 +0100

    Homework2.3VB
    Added Homework2.3VB

---
+++ Homework2.3VisualBasiccorrect/Homework2.3VisualBasiccorrect/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(18, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "GetCSVfile"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(18, 59)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(771, 64)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(149, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(107, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Mean"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(18, 129)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(768, 309)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Homework2.3VisualBasiccorrect/Homework2.3VisualBasiccorrect/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+    Public Class Student
+        Public Name As String
+        Public Height As Double
+
+    End Class
+
+    Dim HeightMean As New List(Of Double)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Path As String = "C:\StudentClass.csv"
+        Using R As New StreamReader(Path)
+            Do While Not R.EndOfStream
+
+                Dim Line As String = R.ReadLine()
+                Dim Value() As String = Line.Split(",".ToCharArray, StringSplitOptions.None)
+                Me.TextBox2.AppendText(Line & Environment.NewLine)
+                Try
+                    HeightMean.Add(CDbl(Value(1)))
+                Catch ex As Exception
+                    Continue Do
+                End Try
+
+            Loop
+
+        End Using
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.TextBox1.AppendText(HeightMean.Average() & Environment.NewLine)
+    End Sub
+End Class
