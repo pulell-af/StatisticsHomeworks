a9c926f84dd526130a44a57a0e2425157f0a7c50
pulell-af <119008536+pulell-af@users.noreply.github.com>
2022-12-03 18:49:54 +0100

    Homework4
    Added Homework4

---
+++ Homework4Csharp/Statistics_Homework4_C/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Statistics_Homework4_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Absolute Frequency";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(548, 181);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(597, 145);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(83, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Relative Frequency";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(597, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Normalized Frequency";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(597, 390);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Histogram";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.Control;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(34, 254);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.Legend = "Legend1";
+            series1.Name = "Distributions";
+            series1.YValuesPerPoint = 6;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(548, 172);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(34, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 36);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "All";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(597, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Percentage";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Homework4Csharp/Statistics_Homework4_C/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Statistics_Homework4_C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        double probabilityOfSuccess = 0.5;
+        int simulations = 0;
+        public Bitmap b;
+        public Graphics g;
+        public Chart c;
+        public int heads = 0;                                                    // A trial toss is considered head if toss >= success prob
+        public int tails = 0;
+        public double head_rf = 0;                                               // heads & tails realative frequency 
+        public double tails_rf = 0;
+        public double head_nf = 0;                                               // heads & tails normalized frequency
+        public double tail_nf = 0;
+        public int count = 0;
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            this.g = Graphics.FromImage(b);
+            this.c = new Chart();
+        }
+
+
+        public int resizeX(double minX, double maxX, int W, double x)
+        {
+
+            return Convert.ToInt32(((x - minX) * W) / (maxX - minX));
+
+        }
+        public int resizeY(double minY, double maxY, int H, double y)
+        {
+            return Convert.ToInt32(H - ((y - minY) * H) / (maxY - minY));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text == "Absolute Frequency")
+            {
+                this.numericUpDown1.Enabled = false;
+                this.button1.Text = "Stop";
+                probabilityOfSuccess = ((Convert.ToDouble(numericUpDown1.Value)) / 100);
+                timer1.Start();
+            }
+            else
+            {
+                this.button1.Text = "Absolute Frequency";
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            int trials = 1000;
+            int flip= 0;
+            simulations+=1;
+
+            Point[] points1 = new Point[trials];
+
+            for (int i = 0; i < trials; i++)
+            {
+                double uniform = r.NextDouble();
+                if (uniform < probabilityOfSuccess) { heads++; head_rf = flip * trials / (i + 1); head_nf = flip * (Math.Sqrt(trials)) / (Math.Sqrt(i + 1)); flip++; }
+                else { tails++; tails_rf = (trials - flip) * trials / (i + 1); tail_nf = ((trials - flip) * (Math.Sqrt(trials))) / (Math.Sqrt(i + 1)); }
+
+                Point dist_1 = new Point(); 
+                dist_1.Y = resizeY(0, trials, pictureBox1.Height, flip);
+                dist_1.X = resizeX(0, trials, pictureBox1.Width, i);
+
+
+                points1[i] = dist_1;
+
+            }
+
+            this.g.DrawLines(Pens.Pink, points1);
+            this.pictureBox1.Image = b;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text == "Relative Frequency")
+            {
+                this.numericUpDown1.Enabled = false;
+                this.button2.Text = "Stop";
+                probabilityOfSuccess = ((Convert.ToDouble(numericUpDown1.Value)) / 100);
+                timer2.Start();
+            }
+            else
+            {
+                this.button2.Text = "Relative Frequency";
+                timer2.Stop();
+            }
+
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int trials2 = 1000;
+            int flip2 = 0;
+            simulations+=1;
+            Point[] points2 = new Point[trials2];
+            for (int i = 0; i < trials2; i++)
+            {
+                double uniform = r.NextDouble();
+                if (uniform < probabilityOfSuccess) flip2++;
+                Point dist_2 = new Point(); 
+                dist_2.Y = resizeY(0, trials2, pictureBox1.Height, flip2 * trials2 / (i + 1));
+                dist_2.X = resizeX(0, trials2, pictureBox1.Width, i);
+                points2[i] = dist_2;
+            }
+            this.g.DrawLines(Pens.DeepSkyBlue, points2);
+            this.pictureBox1.Image = b;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.button3.Text == "Normalized Frequency")
+            {
+                this.numericUpDown1.Enabled = false;
+                this.button3.Text = "Stop";
+                probabilityOfSuccess = ((Convert.ToDouble(numericUpDown1.Value)) / 100);
+                timer3.Start();
+            }
+            else
+            {
+                this.button3.Text = "Normalized Frequency";
+                timer3.Stop();
+            }
+
+        }
+
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int trials3 = 1000;
+            int flip3 = 0;
+            simulations+=1;
+
+            Point[] points3 = new Point[trials3];
+
+            for (int i = 0; i < trials3; i++)
+            {
+                double uniform = r.NextDouble();
+                if (uniform < probabilityOfSuccess) flip3++;
+                Point dist_3 = new Point();
+                dist_3.Y = resizeY(0, trials3 * probabilityOfSuccess, pictureBox1.Height, flip3 * (Math.Sqrt(trials3)) / (Math.Sqrt(i + 1)));
+                dist_3.X = resizeX(0, trials3, pictureBox1.Width, i);
+                points3[i] = dist_3;
+
+            }
+            this.g.DrawLines(Pens.OrangeRed, points3);
+            this.pictureBox1.Image = b;
+        }
+
+        private void fillChart()
+        {
+
+            chart1.Series["Distributions"].Points.AddXY("N° Heads", heads);
+            chart1.Series["Distributions"].Points.AddXY("N° Tails", tails);
+            chart1.Series["Distributions"].Points.AddXY("Heads RF", head_rf);
+            chart1.Series["Distributions"].Points.AddXY("Tails RF", tails_rf);
+            chart1.Series["Distributions"].Points.AddXY("Heads NF", head_rf);
+            chart1.Series["Distributions"].Points.AddXY("Tails NF", tails_rf);
+
+            string c = "Distribution Chart " + count.ToString();
+            chart1.Titles.Add(c);
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (this.button4.Text == "Histogram")
+            {
+                fillChart();
+                this.button4.Text = "Clear";
+
+            }
+            else
+            {
+
+                this.chart1.Series.Clear();
+                this.button4.Text = "Histogram";
+                heads = 0;
+                tails = 0;
+                head_rf = 0;
+                tails_rf = 0;
+                head_nf = 0;
+                tail_nf = 0;
+                simulations = 0;
+
+            }
+        }
+
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (this.button5.Text == "All")
+            {
+                this.numericUpDown1.Enabled = false;
+                this.button5.Text = "Stop";
+                probabilityOfSuccess = ((Convert.ToDouble(numericUpDown1.Value)) / 100);
+                timer3.Start();
+                timer1.Start();
+                timer2.Start();
+            }
+            else
+            {
+                this.button5.Text = "All";
+                timer3.Stop();
+                timer1.Stop();
+                timer2.Stop();
+            }
+
+        }
+    }
+}
